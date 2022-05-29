2fb357d2ca9e822ab3c3281daad9a99b7f6e785f
Amy McCabe <mcca0168@umn.edu>
2022-05-28 21:05:27 -0500

    high scores list

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,16 @@ var option2BtnEl = document.getElementById("option2-btn");
 var option3BtnEl = document.getElementById("option3-btn");
 var option4BtnEl = document.getElementById("option4-btn");
 var hsListDivEl = document.getElementById("hs-list");
+var viewHSBtn = document.getElementById("view-hs");
+var hsTitleEl = document.getElementById("hs-title");
+var playAgainBtn = document.getElementById("play-again");
+var submitBtnEl = document.getElementById("submit-btn");
 
 document.createElement("p");
 var lowerPEl = document.createElement("p");
 lowerPEl.className = "correct-incorrect";
 
 introAnswersEl.addEventListener("click", selectedAnswer);
-
-var submitBtnEl = document.getElementById("submit-btn");
-
-
 
 var currentQuestionIndex = 0;
 
@@ -73,69 +73,69 @@ const questionArray = [
       {text: "  !  ", correct:false}
     ]
   },
-  //{
-  //  question: "Javascript comments start with:",
-  //  answers: [
-  //    {text: "  <!  ", correct:false},
-  //    {text: "  {  ", correct: false},
-  //    {text: "  **  ", correct:false},
-  //    {text: "  //  ", correct:true}
-  //  ]
-  //},
-  //{
-  //  question: "Which does not declare a Javascript variable?",
-  //  answers: [
-  //    {text: "var", correct:false},
-  //    {text: "let", correct:false},
-  //    {text: "make", correct:true},
-  //    {text: "const", correct:false}
-  //  ]
-  //},
-  //{
-  //  question: "Which symbols enclose an object?",
-  //  answers: [
-  //    {text: " { } ", correct:true},
-  //    {text: " // ", correct:false},
-  //    {text: " [ ] ", correct:false},
-  //    {text: " ( ) ", correct:false}
-  //  ]
-  //}, 
-  //{
-  //  question: "Math.random(); will return a number:",
-  //  answers: [
-  //    {text: "between 1 & 10", correct:false},
-  //    {text: "that is >=0 and <1", correct:true},
-  //    {text: "between -100 and 100", correct:false},
-  //    {text: "that is >=0 and <=1", correct:false}
-  //  ]
-  //},
-  //{
-  //  question: "Arrays in Javascript can be used to store:",
-  //  answers: [
-  //    {text: "Numbers and strings", correct:false},
-  //    {text: "Other arrays", correct:false},
-  //    {text: "Booleans", correct:false},
-  //    {text: "All of the above", correct:true}
-  //  ]
-  //},
-  //{
-  //question: "Which symbols enclose an array?",
-  //  answers: [
-  //    {text: " { } ", correct:false},
-  //    {text: " // ", correct:false},
-  //    {text: " [ ] ", correct:true},
-  //    {text: " ( ) ", correct:false}
-  //  ]
-  //},
-  //{
-  // question: "The Javascript arithmetic operator * means:",
-  //  answers: [
-  //    {text: "Addition", correct:false},
-  //    {text: "Subtraction", correct:false},
-  //    {text: "Multiplication", correct:true},
-  //    {text: "Division", correct:false}
-  //  ]
-  //},
+  {
+    question: "Javascript comments start with:",
+    answers: [
+      {text: "  <!  ", correct:false},
+      {text: "  {  ", correct: false},
+      {text: "  **  ", correct:false},
+      {text: "  //  ", correct:true}
+    ]
+  },
+  {
+    question: "Which does not declare a Javascript variable?",
+    answers: [
+      {text: "var", correct:false},
+      {text: "let", correct:false},
+      {text: "make", correct:true},
+      {text: "const", correct:false}
+    ]
+  },
+  {
+    question: "Which symbols enclose an object?",
+    answers: [
+      {text: " { } ", correct:true},
+      {text: " // ", correct:false},
+      {text: " [ ] ", correct:false},
+      {text: " ( ) ", correct:false}
+    ]
+  }, 
+  {
+    question: "Math.random(); will return a number:",
+    answers: [
+      {text: "between 1 & 10", correct:false},
+      {text: "that is >=0 and <1", correct:true},
+      {text: "between -100 and 100", correct:false},
+      {text: "that is >=0 and <=1", correct:false}
+    ]
+  },
+  {
+    question: "Arrays in Javascript can be used to store:",
+    answers: [
+      {text: "Numbers and strings", correct:false},
+      {text: "Other arrays", correct:false},
+      {text: "Booleans", correct:false},
+      {text: "All of the above", correct:true}
+    ]
+  },
+  {
+  question: "Which symbols enclose an array?",
+    answers: [
+      {text: " { } ", correct:false},
+      {text: " // ", correct:false},
+      {text: " [ ] ", correct:true},
+      {text: " ( ) ", correct:false}
+    ]
+  },
+  {
+   question: "The Javascript arithmetic operator * means:",
+    answers: [
+      {text: "Addition", correct:false},
+      {text: "Subtraction", correct:false},
+      {text: "Multiplication", correct:true},
+      {text: "Division", correct:false}
+    ]
+  },
   {
     question: "In Javascript which symbol assigns meaning to a variable?",
     answers: [
@@ -147,43 +147,69 @@ const questionArray = [
   }
 ];
 
-function highScoreCard() {
-  console.log("high scores");
+function clearStorage() {
+  localStorage.clear();
+  hsListDivEl.className = "hide";
+};
+
+function highScoreCard(scores) {
   var scoreListEl = document.createElement("ol");
   hsListDivEl.appendChild(scoreListEl);
   scoreListEl.className = "hs-list";
+
+  const storageScores = localStorage.getItem("scores");
+  const scoresArray = JSON.parse(storageScores);
   
-  var hsListEl1 = document.createElement("li");
-  hsListEl1.className = "hs-list-item";
-  hsListDivEl.appendChild(hsListEl1);
-  hsListEl1.className = "hs-list-item";
-  hsListEl1.textContent = document.querySelector("input[name='initials']").value + ", " + time;
-  var hsListEl2 = document.createElement("li");
-  hsListDivEl.appendChild(hsListEl2);
-  hsListEl2.className = "hs-list-item";
-  var hsListEl3 = document.createElement("li");
-  hsListDivEl.appendChild(hsListEl3);
-  hsListEl3.className = "hs-list-item";
+  scoresArray.forEach(function(e) {
+    var hsListEl1 = document.createElement("li");
+    hsListDivEl.appendChild(hsListEl1);
+    hsListEl1.className = "hs-list-item";
+    hsListEl1.textContent = `${e.initials} ${e.score}`;
+  });
+  hsTitleEl.classList.remove("hide");
+
+  var clearScores = document.createElement("button");
+  clearScores.textContent = "Clear Score List";
+  hsListDivEl.appendChild(clearScores);
+  clearScores.className = "btn";
+  clearScores.addEventListener("click", clearStorage);
 };
 
+ function setHighScores(initials, score) {
+  const storageScores = localStorage.getItem("scores");
+  const item = {initials: initials, score: score};
+  
+  if (storageScores === null) {
+    localStorage.setItem("scores", JSON.stringify([item]));
+  } else {
+    var scoresArray = JSON.parse(storageScores);
+    console.log(scoresArray);
+    scoresArray.push(item);
+    scoresArray.sort(function(a,b) {
+      return b.score - a.score;
+    });
+    scoresArray.length = (scoresArray.length < 3) ? scoresArray.length : 3;
+    console.log(scoresArray);
+    localStorage.setItem("scores", JSON.stringify(scoresArray));
+  }
+ };
 
 function highScores(event, inputText) {
   event.preventDefault();
   var initialInput = document.querySelector("input[name='initials']").value;
   console.log(initialInput + ", " + time);
-  localStorage.setItem(initialInput, time);
-  //upperEl.className = "hide";
-  //lowerEl.className = "hide";
+  var playerScore = time;
   lowerLowerEl.className = "hide";
+  playAgainBtn.classList.remove("hide");
+  playAgainBtn.className = "btn";
+  setHighScores(initialInput, playerScore);
   highScoreCard();
-  
 };
 
 function saveScore() {
   var inputFormEl = document.getElementById("lower-lower");
   inputFormEl.className = "input-form";
-
-  };
+};
 
 function setScore() {
   introAnswersEl.className = "hide";
